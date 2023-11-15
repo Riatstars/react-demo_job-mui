e36645862305854d73ea32e9607e3935ba153c12
riatstars <riatstars@gmail.com>
2023-11-15 13:16:32 +0700

    optimize code

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -15,7 +15,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import { Container, Grid } from '@mui/material';
+import { Container } from '@mui/material';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
